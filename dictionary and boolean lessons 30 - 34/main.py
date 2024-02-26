b5c856191dc1b71b56e5c60863d98b63b567abd5
unknown <masalim122006@gmail.com>
2024-02-26 23:57:05 +0200

    etc

---
+++ dictionary and boolean lessons 30 - 34/main.py
@@ -0,0 +1,59 @@
+# dictionary lesson 1
+# dictionary is js object
+# keys are nums strs
+# user = {
+#     "name" : "mo",
+#     "age":17
+# }
+# print(user)
+# # print(user.get("name"))
+# # print(user["name"])
+# print(user.keys())
+# print(user.values())
+# print(len(user))
+
+# dictionary lesson 2
+
+# clear() same
+
+# update()
+# you can
+# user["language"] = "JavaScript"
+# or with update()
+# user.update({"language":"JavaScript"})
+# copy() same
+
+# dictionary lesson 3
+
+# setdefault()
+# user.setdefault("age",18)
+# user.setdefault("age2",170)
+# print(user)
+
+# popitem() get last item
+# print(user.popitem())
+# print(user)
+
+# items like object.entries() in js
+
+# dict.fromkeys() make obj from keys and values
+
+# boolean lesson 1
+# print(100>=50)
+# bool()
+# none = null
+
+#  boolean lesson 2
+
+# and like &&
+# print(1<2 and 2>=2)
+
+# or like ||
+# print(1>2 or 2>=2)
+
+# not like ! in js
+print(not 1>2)
+
+# same assignment operators
+# same comparion operators
+
